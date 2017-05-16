b3cb64e01b09bec78e9d50030a732bdf9c398c25
Angelo Arenas <angeloarenas31@gmail.com>
2017-05-16 16:01:07 +0800

    Changed order of notifications

--- hrdo_safs/settings.py
+++ hrdo_safs/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
         'NAME': 'hrdodb',
         'USER': 'hrdo_user',
         'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
